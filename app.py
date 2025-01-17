c8b895bdd9d8208292fd7986adc0c62e0ab8b5e5
ekemainai1 <39128782+ekemainai1@users.noreply.github.com>
2025-01-17 17:32:28 +0000

    Add pages and click listeners to buttons

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def ekemai():
     return render_template('home.html')
 
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/pricing")
+def pricing():
+    return render_template('pricing.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
